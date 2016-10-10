803a5acf60508a4c8084de64b21481ba179ca602
NIGHT-CORE <419472477@qq.com>
2016-10-10 20:05:22 +0800

    Update uninstall_fs.sh

--- uninstall_fs.sh
+++ uninstall_fs.sh
@@ -3,6 +3,19 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 install_path=/fs/
 
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS='centos'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='ubuntu'
+    else
+        echo "Not support OS, Please reinstall OS and retry!"
+        exit 1
+    fi
+}
+
 # Uninstall finalspeed
 uninstall_finalspeed(){
 /etc/init.d/finalspeed stop
