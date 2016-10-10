17ad94ae75da7a76cce48c78281f94a9479423bc
NIGHT-CORE <419472477@qq.com>
2016-10-10 19:58:23 +0800

    Update uninstall_fs.sh

--- uninstall_fs.sh
+++ uninstall_fs.sh
@@ -5,14 +5,7 @@ install_path=/fs/
 
 # Uninstall finalspeed
 uninstall_finalspeed(){
-    printf "Are you sure uninstall finalspeed? (y/n) "
-    printf "\n"
-    read -p "(Default: n):" answer
-    if [ -z $answer ]; then
-        answer="n"
-    fi
-    if [ "$answer" = "y" ]; then
-        /etc/init.d/finalspeed stop
+/etc/init.d/finalspeed stop
         checkos
         if [ "$OS" == 'centos' ]; then
             chkconfig --del finalspeed
@@ -22,9 +15,6 @@ uninstall_finalspeed(){
         rm -f /etc/init.d/finalspeed
         rm -rf $install_path
         echo "finalspeed uninstall success!"
-    else
-        echo "uninstall cancelled, Nothing to do"
-    fi
 }
 
 uninstall_finalspeed
